340aff7bcd515d18bf3165dde8cc96649bac3312
Surja1997 <pialisarkar1968@gmail.com>
2021-04-12 11:01:32 +0530

    CRUD completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
   def index
-    @articles=Article.all
+    @articles = Article.all
   end
-  
+
   def show
-    @article=Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,12 +12,38 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article=Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
+    else
+      render :new
     end
-    render :new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
